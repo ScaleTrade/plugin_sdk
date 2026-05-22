66cbbdf8c23b5e573cc005ea506ec925f039ad07
Viktor Semenov <mail@sobix.net>
2026-05-22 16:45:45 +0300

    update SDK

--- include/model/PluginTradeEnums.hpp
+++ include/model/PluginTradeEnums.hpp
@@ -57,7 +57,14 @@ enum class PluginTradeReason : int {
     Gateway = 4,
     Mobile = 5,
     Web = 6,
-    Api = 7
+    Api = 7,
+    Prop = 8,
+    CopyTrade = 9,
+    CopyReward = 10,
+    CashierDeposit = 11,
+    CashierWithdrawal = 12,
+    CashierRefund = 13,
+    CashierChargeback = 14
 };
 
 enum class PluginTradeActivation : int {
