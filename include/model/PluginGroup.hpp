117198c4166bf771d9cc3bf568e6e4c00fc2e4b4
Viktor Semenov <mail@sobix.net>
2026-09-10 14:01:29 +0300

    add

--- include/model/PluginGroup.hpp
+++ include/model/PluginGroup.hpp
@@ -98,5 +98,6 @@ struct PluginGroupRecord {
     int archive_pending_period = 0;
     unsigned int news_languages[8] = {0, 0, 0, 0, 0, 0, 0, 0};
     unsigned int news_languages_total = 0;
-    int reserved[16] = {};
+    int deposit_reversal_policy = 0;
+    int reserved[15] = {};
 };
